4d238ab624d63f87438fd3c728dca61bdeee283c
Patrick <patrick.henninger@contagt.com>
2021-05-21 03:31:14 +0200

    Add package service provider,

---
+++ src/SquadMSServersServiceProvider.php.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace SquadMS\Foundation;
+
+use Illuminate\Support\ServiceProvider;
+
+class SquadMSServersServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
