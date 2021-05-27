021c757ab0f644f8b07f38a6af9704ba04b55d2d
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-27 04:02:04 +0200

    Add missing route file,

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+
+/* Define routes from config */
+foreach (config('sqms-servers.routes.def') as $definition) {
+    /* Create the definitor as an anonymous function */
+    $define = function() use ($definition) {
+        $type = Arr::get($definition, 'type', 'get');
+
+        Route::$type(Arr::get($definition, 'path', '/'), [Arr::get($definition, 'controller'), Arr::get($definition, 'executor', 'show')])->middleware(Arr::get($definition, 'middlewares'))->name(Arr::get($definition, 'name'));
+    };
+
+    if (Arr::get($definition, 'localized', false)) {
+        Route::localized(function() use ($define) {
+            $define();
+        });
+    } else {
+        $define();
+    }
+}
