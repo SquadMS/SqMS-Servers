1c3c83b545592d85e6b97d55f3144a76f1b84fd8
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-27 05:17:46 +0200

    Use Module to register the routes,

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -2,14 +2,9 @@
 
 namespace SquadMS\Servers\Providers;
 
-use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
-use SquadMS\Foundation\Facades\SquadMSAdminMenu as FacadesSquadMSAdminMenu;
 use SquadMS\Foundation\Facades\SquadMSRouter as FacadesSquadMSRouter;
-use SquadMS\Foundation\Facades\SquadMSMenu as FacadesSquadMSMenu;
-use SquadMS\Foundation\Helpers\NavigationHelper;
-use SquadMS\Foundation\Menu\SquadMSMenuEntry;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -30,26 +25,5 @@ class RouteServiceProvider extends ServiceProvider
                 $this->loadRoutesFrom($routesPath . '/web.php');
             });
         });
-
-        /* Public Menu */
-        FacadesSquadMSMenu::register(
-            'main-left',
-            (new SquadMSMenuEntry(Config::get('sqms-servers.routes.def.servers.name'), __('sqms-servers::navigation.servers'), true))
-            ->setActive( fn (SquadMSMenuEntry $link) => NavigationHelper::isCurrentRoute(Config::get('sqms-servers.routes.def.servers.name')) )
-            ->setOrder(200)
-        );
-
-        /* Admin Menu */
-        FacadesSquadMSAdminMenu::register('admin-servers', 200);
-        FacadesSquadMSMenu::prepend('admin-servers', fn () => view('sqms-foundation::components.navigation.heading', [
-            'title'  => 'Server Management',
-        ])->render());
-
-        FacadesSquadMSMenu::register(
-            'admin-servers',
-            (new SquadMSMenuEntry(Config::get('sqms-servers.routes.def.admin-servers.name'), '<i class="bi bi-house-fill"></i> Servers', true))->setView('sqms-foundation::components.navigation.item')
-            ->setActive( fn (SquadMSMenuEntry $link) => NavigationHelper::isCurrentRoute(Config::get('sqms-servers.routes.def.admin-servers.name')) )
-            ->setOrder(200)
-        );
     }
 }
--- src/SquadMSModule.php
+++ src/SquadMSModule.php
@@ -2,6 +2,11 @@
 
 namespace SquadMS\Servers;
 
+use Illuminate\Support\Facades\Config;
+use SquadMS\Foundation\Facades\SquadMSAdminMenu;
+use SquadMS\Foundation\Facades\SquadMSMenu;
+use SquadMS\Foundation\Helpers\NavigationHelper;
+use SquadMS\Foundation\Menu\SquadMSMenuEntry;
 use SquadMS\Foundation\Modularity\Contracts\SquadMSModule as SquadMSModuleContract;
 
 class SquadMSModule extends SquadMSModuleContract {
@@ -18,5 +23,40 @@ class SquadMSModule extends SquadMSModuleContract {
     static function publishAssets() : void
     {
         //
+    }
+
+    static function registerAdminMenus() : void
+    {
+        SquadMSAdminMenu::register('admin-servers', 200);
+    }
+
+    static function registerMenuEntries(string $menu) : void
+    {
+        switch ($menu) {
+            case 'main-left':
+                SquadMSMenu::register(
+                    'main-left',
+                    (new SquadMSMenuEntry(Config::get('sqms-servers.routes.def.servers.name'), __('sqms-servers::navigation.servers'), true))
+                    ->setActive( fn (SquadMSMenuEntry $link) => NavigationHelper::isCurrentRoute(Config::get('sqms-servers.routes.def.servers.name')) )
+                    ->setOrder(200)
+                );
+
+                break;
+
+            case 'admin-servers':
+                /* Admin Menu */
+                SquadMSMenu::prepend('admin-servers', fn () => view('sqms-foundation::components.navigation.heading', [
+                    'title'  => 'Server Management',
+                ])->render());
+
+                SquadMSMenu::register(
+                    'admin-servers',
+                    (new SquadMSMenuEntry(Config::get('sqms-servers.routes.def.admin-servers.name'), '<i class="bi bi-house-fill"></i> Servers', true))->setView('sqms-foundation::components.navigation.item')
+                    ->setActive( fn (SquadMSMenuEntry $link) => NavigationHelper::isCurrentRoute(Config::get('sqms-servers.routes.def.admin-servers.name')) )
+                    ->setOrder(200)
+                );
+
+                break;
+        }
     }
 }
