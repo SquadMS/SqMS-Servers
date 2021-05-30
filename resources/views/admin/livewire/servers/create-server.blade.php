6bc129e50642fe6040e2df7b676b45e0e5f222a8
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-30 05:24:03 +0200

    Fix,

--- resources/views/admin/livewire/servers/create-server.blade.php
+++ resources/views/admin/livewire/servers/create-server.blade.php
@@ -3,39 +3,39 @@
         Create
     </x-sqms-foundation::button>
 
-    <x-sqms-foundation::dialog-modal wire:model="showModal" class="fs-base">
+    <x-sqms-foundation::dialog-modal model="showModal" class="fs-base">
         <x-slot name="title">
             Create Server
         </x-slot>
     
         <x-slot name="content">
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="New server name" aria-label="New server name" wire:model.lazy="server.name">
+                <input type="text" class="form-control" placeholder="New server name" aria-label="New server name" wire:dirty.class="border-warning" wire:model.stop="server.name">
             </div>
 
             <div class="form-check form-switch mb-3">
-                <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" {{ $server->account_playtime ? 'checked' : '' }} wire:model="server.account_playtime" value="1">
+                <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" {{ $server->account_playtime ? 'checked' : '' }} wire:dirty.class="border-warning" wire:model.lazy="server.account_playtime" value="1">
                 <label class="form-check-label" for="flexSwitchCheckDefault">Account playtime</label>
             </div>
 
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="New server host" aria-label="New server host" wire:model.lazy="server.host">
+                <input type="text" class="form-control" placeholder="New server host" aria-label="New server host" wire:dirty.class="border-warning" wire:model.lazy="server.host">
             </div>
 
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="New server port" aria-label="New server port" wire:model.lazy="server.game_port">
+                <input type="text" class="form-control" placeholder="New server port" aria-label="New server port" wire:dirty.class="border-warning" wire:model.lazy="server.game_port">
             </div>
 
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="New server query port" aria-label="New server query port" wire:model.lazy="server.query_port">
+                <input type="text" class="form-control" placeholder="New server query port" aria-label="New server query port" wire:dirty.class="border-warning" wire:model.lazy="server.query_port">
             </div>
 
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="New server rcon password" aria-label="New server rcon password" wire:model.lazy="server.rcon_password">
+                <input type="text" class="form-control" placeholder="New server rcon password" aria-label="New server rcon password" wire:dirty.class="border-warning" wire:model.lazy="server.rcon_password">
             </div>
 
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="New server rcon port" aria-label="New server rcon port" wire:model.lazy="server.rcon_port">
+                <input type="text" class="form-control" placeholder="New server rcon port" aria-label="New server rcon port" wire:dirty.class="border-warning" wire:model.lazy="server.rcon_port">
             </div>
         </x-slot>
     
