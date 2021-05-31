428980dce7e9b3161125c64192e269fd4f887434
Patrick Henninger <patrick.henninger@contagt.com>
2021-05-31 20:40:57 +0200

    Remove redundant dependency, Add Server policy, Register permissions,

---
+++ src/Providers/AuthServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SquadMS\Servers\Providers;
+
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use SquadMS\Foundation\Policies\ServerPolicy;
+use SquadMS\Servers\Models\Server;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        Server::class => ServerPolicy::class,
+    ];
+
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        /* Permissions */
+        $this->registerPolicies();
+    }
+}
