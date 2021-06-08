046a07334e74beb984d4b4d636b95ca6e8cb1465
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-08 18:20:40 +0200

    Make use of new webRoutes helper,

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,6 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Route;
+use SquadMS\Foundation\Facades\SquadMSRouter;
 
-/* Define routes from config */
-foreach (Config::get('sqms-servers.routes.def') as $definition) {
-    /* Create the definitor as an anonymous function */
-    $define = function() use ($definition) {
-        $type = Arr::get($definition, 'type', 'get');
-
-        Route::$type(Arr::get($definition, 'path', '/'), [Arr::get($definition, 'controller'), Arr::get($definition, 'executor', 'show')])->middleware(Arr::get($definition, 'middlewares'))->name(Arr::get($definition, 'name'));
-    };
-
-    if (Arr::get($definition, 'localized', false)) {
-        Route::localized(function() use ($define) {
-            $define();
-        });
-    } else {
-        $define();
-    }
-}
+SquadMSRouter::webRoutes(Config::get('sqms-servers.routes.def', []));
