36c71773322360e44cfc94cd65f749b0d474d3b7
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-08 18:55:06 +0200

    Fix missing trait

--- src/Admin/Http/Controllers/ServersController.php
+++ src/Admin/Http/Controllers/ServersController.php
@@ -5,9 +5,12 @@ namespace SquadMS\Servers\Admin\Http\Controllers;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\View;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class ServersController extends Controller
 {
+    use AuthorizesRequests;
+    
     /**
      * Shows the profile page
      *
