ed74c019d804fc8f8c9a3be3f603d71eb587d1c9
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-11 23:09:24 +0200

    Add api middleware to api routes

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use SquadMS\Servers\Http\Controllers\APIServersController;
 
 /* It is important to use the sqms-worker-auth Middleware here to prevent unauthorized access! */
-Route::group(['prefix' => 'rcon-worker', 'as' => 'worker.', 'middleware' => ['sqms-worker-auth']], function () {
+Route::group(['prefix' => 'rcon-worker', 'as' => 'worker.', 'middleware' => ['api', 'sqms-worker-auth']], function () {
     /* Get Server (connection) info */
     Route::get('servers', [APIServersController::class, 'servers']);
 
