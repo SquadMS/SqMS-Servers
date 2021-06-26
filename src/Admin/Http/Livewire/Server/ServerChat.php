2f552b9f63f53b9c2e3293087286e6e2838919b9
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-27 01:34:41 +0200

    Fix echo listener

--- src/Admin/Http/Livewire/Server/ServerChat.php
+++ src/Admin/Http/Livewire/Server/ServerChat.php
@@ -16,7 +16,7 @@ class ServerChat extends Component
     use AuthorizesRequests;
 
     protected $listeners = [
-        'echo:SquadMS\\Servers\\Events\\ServerChatMessageCreated' => '$refresh',
+        'echo:server-chat.SquadMS\\Servers\\Events\\ServerChatMessageCreated' => '$refresh',
     ];
 
     public Server $server;
