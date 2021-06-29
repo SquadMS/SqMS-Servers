5294be954aa66b31a0e811574a0ba2ef294f687a
Patrick Henninger <patrick.henninger@contagt.com>
2021-06-29 21:39:02 +0200

    Refactor chat

--- resources/views/admin/livewire/server/chat.blade.php
+++ resources/views/admin/livewire/server/chat.blade.php
@@ -1,11 +1,15 @@
 <div class="card">
     <style>
-        .chat-messages {
+        #chat-messages {
             display: grid;
             grid-template-columns: fit-content(100%) fit-content(100%) 4fr;
             grid-template-rows: 1fr;
             gap: 0 0;
             grid-template-areas: ". .";
+        }
+
+        .chat-scroll.loading {
+
         }
     </style>
 
@@ -15,72 +19,114 @@
         <hr>
 
         <div 
-            class="chat-scroll vh-100 overflow-auto bg-dark text-white mb-3" 
-            style="max-height: 60vh"
+            class="position-relative"
             x-data="{
-                scrollLock: true,
-                observe () {
-                    let observer = new IntersectionObserver((entries) => {
-                        entries.forEach(entry => {
-                            if (entry.isIntersecting) {
-                                @this.call('loadOld')
-                            }
-                        })
-                    }, {
-                        root: null
-                    })
-
-                    // Start scrolled down
-                    this.$el.scrollTop = this.$el.scrollHeight
-        
-                    // Check if element is scrolled in view
-                    observer.observe(document.getElementById('chatLoadingBefore'))
-
-                    // Keep scrolled down if locked
-                    @this.on('loaded-new', (foo, bar) => {
-                        if (scrollLock) {
-                            this.$el.scrollTop = this.$el.scrollHeight
-                        }
-                    })
-
-                    // Listen for scroll and toggle the scrollLock
-                    this.$el.addEventListener('scroll', e => {
-                        scrollLock = this.$el.scrollTop === this.$el.scrollHeight - this.$el.offsetHeight;
-                    })
-                }
+                scrollLock: @entangle('scrollLock'),
+                hasOld: @entangle('hasOld'),
+                hasNew: @entangle('hasNew'),
+                loadingOld: false,
+                loadingNew: false,
             }"
-            x-init="observe"
+            x-init="() => {
+
+                debugger
+
+                const scrollArea = $el.firstElementChild
+                const scrollContent = document.getElementById('chat-messages')
+
+                // Start scrolled down
+                scrollArea.scrollTop = scrollArea.scrollHeight
+
+                // Keep scrolled down if locked
+                @this.on('loaded-new', (foo, bar) => {
+                    if (scrollLock) {
+                        scrollArea.scrollTop = scrollArea.scrollHeight
+                    }
+                })
+
+                // Listen for scroll and toggle the scrollLock
+                let lastScrollTop = scrollArea.scrollTop;
+                scrollArea.addEventListener('scroll', e => {
+                    const scrollPc = Math.ceil( (scrollArea.scrollTop / (scrollArea.scrollHeight - scrollArea.offsetHeight)) * 100 )
+
+                    const direction = lastScrollTop > scrollArea.scrollTop
+                    lastScrollTop = scrollArea.scrollTop;
+
+                    if (direction && scrollPc <= 20 && hasOld) {
+                        if (!loadingOld) {
+                            loadingOld = true
+
+                            const prevFirstChild = scrollContent.firstElementChild
+                            @this.call('loadOld').finally(() => {
+                                scrollArea.scrollTop += prevFirstChild.firstElementChild.offsetTop
+
+                                loadingOld = false
+                            })
+                            
+                        }
+                    } else if (!direction && scrollPc >= 80 && hasNew) {
+                        if (!loadingNew) {
+                            loadingNew = true
+                            @this.call('loadNew').finally(() => {
+                                loadingNew = false
+                            })
+                        }
+                    }
+                    
+                    if (scrollPc === 100 && !hasNew) {
+                        scrollLock = true;
+                    } else {
+                        scrollLock = false;
+                    }
+                })
+            }"
         >
-            @if ($hasOld)
-                <div id="chatLoadingBefore" class="align-items-center justify-content-center p-3">
+            <div class="chat-scroll vh-100 position-relative overflow-auto bg-dark text-white mb-3" style="max-height: 60vh; transform: translateZ(0);">
+                <div class="position-fixed w-100 h-100 d-flex justify-content-center align-items-center bg-transparent-dark-500">
                     <div class="spinner-border" role="status">
-                        <span class="visually-hidden">Loading more Chat-Messages...</span>
+                        <span class="visually-hidden">Loading Chat-Messages...</span>
                     </div>
                 </div>
-            @endif
 
-            <div class="chat-messages">
-                @foreach ($messages as $message)
-                    <div class="d-contents" id="message-{{ $message->id }}">
-                        <div id="message-time-{{ $message->id }}" class="p-1 p-md-2 border border-grey">
-                            {{ $message->time_short }}
-                        </div>
-                        <div id="message-type-{{ $message->id }}" class="p-1 p-md-2 border border-grey">
-                            {{ $message->type_formatted }}
-                        </div>
-                        <div id="message-content-{{ $message->id }}" class="text-start flex-fill p-1 p-md-2 border border-grey {{ $message->color_class }}">
-                            @if (!in_array($message->type, ['Camera', 'Warning', 'Kick', 'Ban']))
-                                @if ($message->user)
-                                <a href="{{ $message->user->profile_url }}" target="_BLANK">{{ $message->type !== 'Broadcast' ? $message->user->name : $message->name }}</a>:&nbsp;
-                                @else
-                                {{ $message->name }}:&nbsp;
-                                @endif
-                            @endif
-                            {{ $message->content }}
-                        </div>
+                <div x-show="hasOld" id="chatLoadingOld" class="align-items-center justify-content-center p-3">
+                    <div class="spinner-border" role="status">
+                        <span class="visually-hidden">Loading old Chat-Messages...</span>
                     </div>
-                @endforeach
+                </div>
+
+                <div id="chat-messages">
+                    @foreach ($messages as $message)
+                        <div class="d-contents" id="message-{{ $message->id }}">
+                            <div id="message-time-{{ $message->id }}" class="p-1 p-md-2 border border-grey">
+                                {{ $message->time_short }}
+                            </div>
+                            <div id="message-type-{{ $message->id }}" class="p-1 p-md-2 border border-grey">
+                                {{ $message->type_formatted }}
+                            </div>
+                            <div id="message-content-{{ $message->id }}" class="text-start flex-fill p-1 p-md-2 border border-grey {{ $message->color_class }}">
+                                @if (!in_array($message->type, ['Camera', 'Warning', 'Kick', 'Ban']))
+                                    @if ($message->user)
+                                    <a href="{{ $message->user->profile_url }}" target="_BLANK">{{ $message->type !== 'Broadcast' ? $message->user->name : $message->name }}</a>:&nbsp;
+                                    @else
+                                    {{ $message->name }}:&nbsp;
+                                    @endif
+                                @endif
+                                {{ $message->content }}
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+
+                <div x-show="hasNew" id="chatLoadingNew" class="align-items-center justify-content-center p-3">
+                    <div class="spinner-border" role="status">
+                        <span class="visually-hidden">Loading new Chat-Messages...</span>
+                    </div>
+                </div>
             </div>
+
+            <button x-show=" !scrollLock " role="button" class="btn btn-primary position-absolute me-3 mb-2" style="bottom: 0; right: 0" wire:click="lockScroll" wire:loading.attr="disabled">
+                <i class="bi bi-arrow-down-square-fill"></i>
+            </button>
         </div>
 
         @can ('admin servers moderation broadcast')
--- src/Admin/Http/Livewire/Server/ServerChat.php
+++ src/Admin/Http/Livewire/Server/ServerChat.php
@@ -21,18 +21,24 @@ class ServerChat extends Component
         'echo-private:server-chat,.SquadMS\\Servers\\Events\\ServerChatMessageCreated' => 'loadNew',
     ];
 
+    const MAX_MESSAGES = 100;
+    const PAGE_SIZE = 25;
+
     public Server $server;
 
     public ?Collection $messages = null;
 
+    public bool $scrollLock = true;
+
     public bool $hasOld = true;
+    public bool $hasNew = false;
 
     public string $message = '';
 
     public function mount()
     {
-        $this->messages = $this->getServerChatMessagesQuery()->limit(50)->get()->reverse();
-        $this->hasOld = $this->messages->count() === 50;
+        $this->messages = $this->getServerChatMessagesQuery()->limit(self::PAGE_SIZE)->get()->reverse();
+        $this->hasOld = $this->messages->count() === self::PAGE_SIZE;
     }
 
     public function sendMessage()
@@ -56,16 +62,14 @@ class ServerChat extends Component
             $query->where('id', '<', $oldestMessage->id);
         }
 
-        /* Query old messages and reverse for correct order */
-        $newMessages = $query->limit(50)->get();
+        /* Query old messages */
+        $newMessages = $query->limit(self::PAGE_SIZE)->get();
 
         /* Determine if there are any old messages left */
-        $this->hasOld = $newMessages->count() === 50;
+        $this->hasOld = $newMessages->count() === self::PAGE_SIZE;
 
         /* Prepend old messages to the message list */
-        foreach ($newMessages as $message) {
-            $this->messages->prepend($message);
-        }
+        $this->prependMessages($newMessages);
 
         /* Magic event to refresh the component */
         $this->emitSelf('refreshComponent');
@@ -73,16 +77,31 @@ class ServerChat extends Component
 
     public function loadNew()
     {
-        /* Get the query builder for the messages */
+        /** @var \Illuminate\Database\Query\Builder Get the query builder for the messages and order to oldest */
         $query = $this->getServerChatMessagesQuery();
 
         /* Only select messages where id is bigger than oldest message */
         if (($newestMessage = $this->messages->last())) {
             $query->where('id', '>', $newestMessage->id);
+        }  
+
+        /* Initialize an empty collection to keep type safety */
+        $newMessages = new Collection();
+
+        /* Scroll locked should always load the newest messages */
+        if ($this->scrollLock) {
+            $newMessages = $query->oldest('time')->limit(self::MAX_MESSAGES)->get();
+
+            /* Locked should not have any messages left since it is live */
+            $this->hasNew = false;
+        } else if ($this->messages->count() < self::MAX_MESSAGES) {
+            $newMessages = $query->limit(self::MAX_MESSAGES - $this->messages->count())->get()->reverse();
+
+            /* Determine if there are messages left that do not fit in */
+            $this->hasNew = (clone $query)->count() - $newMessages->count() > self::MAX_MESSAGES;
         }
 
-        /* Push all new messages on the message list */
-        $this->messages = $this->messages->concat($query->get());
+        $this->appendMessages($newMessages);
 
         /* Magic event to refresh the component */
         $this->emitSelf('refreshComponent');
@@ -91,13 +110,54 @@ class ServerChat extends Component
         $this->emitSelf('loaded-new');
     }
 
+    public function lockScroll()
+    {
+        $this->scrollLock = true;
+
+        $this->loadNew();
+    }
+
     public function render()
     {
         return View::make('sqms-servers::admin.livewire.server.chat');
     }
 
+    private function prependMessages(Collection $messages): void
+    {
+        /* Prepend old messages to the message list */
+        foreach ($messages as $message) {
+            /* Remove from end if there are too many messages */
+            if ($this->messages->count() >= self::MAX_MESSAGES) {
+                $this->messages->pop();
+            }
+
+            /* Add the message */
+            $this->messages->prepend($message);
+        }
+    }
+
+    private function appendMessages(Collection $messages): void
+    {
+        /* Determine the best strategy based on the amount of messages */
+        if ($messages->count() === self::MAX_MESSAGES) {
+            /* Directly assign it as we do not need old messages */
+            $this->messages = $messages;
+        } else {
+            /* Push all new messages on the message list */
+            foreach ($messages as $message) {
+                /* Remove from start if there are too many messages */
+                if ($this->messages->count() >= self::MAX_MESSAGES) {
+                    $this->messages->shift();
+                }
+
+                /* Add the message to the end */
+                $this->messages->push($message);
+            }
+        }
+    }
+
     private function getServerChatMessagesQuery(): HasMany
     {
-        return $this->server->serverChatMessages()->latest();
+        return $this->server->serverChatMessages()->latest('time');
     }
 }
