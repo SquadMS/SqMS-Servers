2911455c27587c60cb27fcbc3bc6c100eb7ff230
Patrick <patrick.henninger@contagt.com>
2021-08-20 23:12:55 +0200

    Update RCONWorkerCommandRunner.php

--- src/RCONCommandRunners/RCONWorkerCommandRunner.php
+++ src/RCONCommandRunners/RCONWorkerCommandRunner.php
@@ -18,290 +18,216 @@ class RCONWorkerCommandRunner implements ServerCommandRunner
     }
 
     /**
-     * ListSquads command. Returns an array
-     * of Teams containing Squads. The output
-     * can be given to the listPlayers method
-     * to add and reference the Player instances.
-     *
-     * @throws \DSG\SquadRCON\Exceptions\RConException
-     *
-     * @return Team[]
+     * @inheritDoc
      */
-    public function listSquads(): string
+    public function listSquads() : string
     {
         return $this->rcon('ListSquads');
     }
 
     /**
-     * ListPlayers command, returns an array
-     * of Player instances. The output of
-     * ListSquads can be piped into it to
-     * assign the Players to their Team/Squad.
-     *
-     * @throws \DSG\SquadRCON\Exceptions\RConException
-     *
-     * @return string
+     * @inheritDoc
      */
-    public function listPlayers(): string
+    public function listPlayers() : string
     {
         /* Execute the ListPlayers command and get the response */
         return $this->rcon('ListPlayers');
     }
 
     /**
-     * ListDisconnectedPlayers command, returns an array
-     * of disconnected Player instances.
-     *
-     * @throws \DSG\SquadRCON\Exceptions\RConException
-     *
-     * @return string
+     * @inheritDoc
      */
-    public function listDisconnectedPlayers(): string
+    public function listDisconnectedPlayers() : string
     {
         return $this->rcon('AdminListDisconnectedPlayers');
     }
 
     /**
-     * AdmiNkick command.
-     * Kick a Player by Name or Steam64ID.
-     *
-     * @param string $nameOrSteamId
-     * @param string $reason
-     *
-     * @throws \DSG\SquadRCON\Exceptions\RConException
-     *
-     * @return bool
+     * @inheritDoc
      */
-    public function adminKick(string $nameOrSteamId, string $reason = ''): bool
+    public function adminKick(string $nameOrSteamId, string $reason = '') : bool
     {
-        return $this->_consoleCommand('AdminKick', $nameOrSteamId.' '.$reason, 'Kicked player ');
+        return $this->_consoleCommand('AdminKick', $nameOrSteamId . ' ' . $reason, 'Kicked player ');
     }
 
     /**
-     * AdminKickById command.
-     * Broadcasts the given message on the server.
-     *
-     * @param int    $id
-     * @param string $reason
-     *
-     * @throws \DSG\SquadRCON\Exceptions\RConException
-     *
-     * @return bool
+     * @inheritDoc
      */
-    public function adminKickById(int $id, string $reason = ''): bool
+    public function adminKickById(int $id, string $reason = '') : bool
     {
-        return $this->_consoleCommand('AdminKickById', $id.' '.$reason, 'Kicked player ');
+        return $this->_consoleCommand('AdminKickById', $id . ' ' . $reason, 'Kicked player ');
     }
 
     /**
-     * AdminBan command.
-     * Bans the given Player from the Server.
-     *
-     * @param string $msg
-     * @param string $reason
-     *
-     * @throws \DSG\SquadRCON\Exceptions\RConException
-     *
-     * @return bool
+     * @inheritDoc
      */
-    public function adminBan(string $nameOrSteamId, string $duration = '1d', string $reason = ''): bool
+    public function adminBan(string $nameOrSteamId, string $duration = '1d', string $reason = '') : bool
     {
-        return $this->_consoleCommand('AdminBan', $nameOrSteamId.' '.$duration.' '.$reason, 'Banned player ');
+        return $this->_consoleCommand('AdminBan', $nameOrSteamId . ' ' . $duration . ' ' . $reason, 'Banned player ');
     }
 
     /**
-     * AdminBanById command.
-     * Bans the given Player from the Server.
-     *
-     * @param int    $id
-     * @param string $reason
-     *
-     * @throws \DSG\SquadRCON\Exceptions\RConException
-     *
-     * @return bool
+     * @inheritDoc
      */
-    public function adminBanById(int $id, string $duration = '1d', string $reason = ''): bool
+    public function adminBanById(int $id, string $duration = '1d', string $reason = '') : bool
     {
-        return $this->_consoleCommand('AdminBanById', $id.' '.$duration.' '.$reason, 'Banned player ');
+        return $this->_consoleCommand('AdminBanById', $id . ' ' . $duration . ' ' . $reason, 'Banned player ');
     }
 
     /**
-     * ShowCurrentMap command.
-     * Gets the current level and layer.
-     *
-     * @throws \DSG\SquadRCON\Exceptions\RConException
-     *
-     * @return array
+     * @inheritDoc
      */
-    public function showCurrentMap(): string
+    public function showCurrentMap() : string
     {
         return $this->rcon('ShowCurrentMap');
     }
 
     /**
-     * ShowNextMap command.
-     * Gets the current and next map.
-     *
-     * @throws \DSG\SquadRCON\Exceptions\RConException
-     *
-     * @return array
+     * @inheritDoc
      */
-    public function showNextMap(): string
+    public function showNextMap() : string
     {
         return $this->rcon('ShowNextMap');
     }
 
     /**
-     * AdminBroadcast command.
-     * Broadcasts the given message on the server.
-     *
-     * @param string $msg
-     *
-     * @throws \DSG\SquadRCON\Exceptions\RConException
-     *
-     * @return bool
+     * @inheritDoc
      */
-    public function adminBroadcast(string $msg): bool
+    public function adminBroadcast(string $msg) : bool
     {
         return $this->_consoleCommand('AdminBroadcast', $msg, 'Message broadcasted');
     }
 
     /**
-     * AdminRestartMatch command.
-     * Broadcasts the given message on the server.
-     *
-     * @return bool
+     * @inheritDoc
      */
-    public function adminRestartMatch(): bool
+    public function adminRestartMatch() : bool
     {
         return $this->_consoleCommand('AdminRestartMatch', '', 'Game restarted');
     }
 
     /**
-     * AdminRestartMatch command.
-     * Broadcasts the given message on the server.
-     *
-     * @return bool
+     * @inheritDoc
      */
-    public function adminEndMatch(): bool
+    public function adminEndMatch() : bool
     {
         return $this->_consoleCommand('AdminEndMatch', '', 'Match ended');
     }
 
     /**
-     * AdminSetMaxNumPlayers command.
-     * Sets the max amount of players (public).
-     *
-     * @param int $slots How many public slots ther should be.
-     *
-     * @throws \DSG\SquadRCON\Exceptions\RConException
-     *
-     * @return bool
+     * @inheritDoc
      */
-    public function adminSetMaxNumPlayers(int $slots): bool
+    public function adminSetMaxNumPlayers(int $slots) : bool
     {
-        return $this->_consoleCommand('AdminSetMaxNumPlayers', $slots, 'Set MaxNumPlayers to '.$slots);
+        return $this->_consoleCommand('AdminSetMaxNumPlayers', $slots, 'Set MaxNumPlayers to ' . $slots);
     }
 
     /**
-     * AdminSetServerPassword command.
-     * Sets the password of the server.
-     *
-     * @param string $password
-     *
-     * @throws \DSG\SquadRCON\Exceptions\RConException
-     *
-     * @return bool
+     * @inheritDoc
      */
-    public function adminSetServerPassword(string $password): bool
+    public function adminSetServerPassword(string $password) : bool
     {
-        return $this->_consoleCommand('AdminSetServerPassword', $password, 'Set server password to '.$password);
+        return $this->_consoleCommand('AdminSetServerPassword', $password, 'Set server password to ' . $password);
     }
 
     /**
-     * AdminChangeMap command
-     * Immediately changes the current map to the given map.
-     *
-     * @param string $map
-     *
-     * @throws \DSG\SquadRCON\Exceptions\RConException
-     *
-     * @return bool
+     * @inheritDoc
      */
-    public function adminChangeMap(string $map): bool
+    public function adminChangeLevel(string $level) : bool
     {
-        return $this->_consoleCommand('AdminChangeMap', $map, 'Changed map to');
+        return $this->_consoleCommand('AdminChangeLevel', $level, 'Change level to');
     }
 
     /**
-     * AdminSetNextMap command.
-     * Temporarily overwrites the next map in the
-     * MapRotations, effecively changing the next map.
-     *
-     * @param string $map
-     *
-     * @throws \DSG\SquadRCON\Exceptions\RConException
-     *
-     * @return bool
+     * @inheritDoc
      */
-    public function adminSetNextMap(string $map): bool
+    public function adminSetNextLevel(string $level) : bool
     {
-        return $this->_consoleCommand('AdminSetNextMap', $map, 'Set next map to');
+        return $this->_consoleCommand('AdminSetNextLevel', $level, 'Set next level to');
     }
 
     /**
-     * AdminSlomo command.
-     * Sets the game speed with the AdminSlomo
-     * command. Providing no parameter will set
-     * the speed to default.
-     *
-     * @param float $timeDilation
-     *
-     * @throws \DSG\SquadRCON\Exceptions\RConException
-     *
-     * @return bool
+     * @inheritDoc
      */
-    public function adminSlomo(float $timeDilation = 1.0): bool
+    public function adminChangeLayer(string $layer) : bool
+    {
+        return $this->_consoleCommand('AdminChangeLayer', $layer, 'Change layer to');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function adminSetNextLayer(string $layer) : bool
+    {
+        return $this->_consoleCommand('AdminSetNextLayer', $layer, 'Set next layer to');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function adminVoteLevel(string $levels) : bool
+    {
+        return $this->_consoleCommand('AdminVoteLevel', $levels, 'TODO');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function adminVoteLayer(string $layers) : bool
+    {
+        return $this->_consoleCommand('AdminVoteLayer', $layers, 'TODO');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function adminVoteNextLevel(string $levels) : bool
+    {
+        return $this->_consoleCommand('AdminVoteNextLevel', $levels, 'TODO');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function adminVoteNextLayer(string $layer) : bool
+    {
+        return $this->_consoleCommand('AdminVoteNextLayer', $layer, 'TODO');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function adminVote(string $name, string $choices) : bool
+    {
+        return $this->_consoleCommand('AdminVote', $name + ' ' + $choices, 'TODO');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function adminSlomo(float $timeDilation = 1.0) : bool
     {
         return $this->_consoleCommand('AdminSlomo', $timeDilation);
     }
 
     /**
-     * AdminForceTeamChange command.
-     * Forces a player to the opposite team
-     * by providing the name or steamid.
-     *
-     * @param string $nameOrSteamId
-     *
-     * @return bool
+     * @inheritDoc
      */
-    public function adminForceTeamChange(string $nameOrSteamId): bool
+    public function adminForceTeamChange(string $nameOrSteamId) : bool
     {
         return $this->_consoleCommand('AdminForceTeamChange', $nameOrSteamId, 'Forced team change for player ');
     }
 
     /**
-     * AdminForceTeamChangeById command.
-     * Forces a player to the opposite team
-     * by providing the ingame Player id.
-     *
-     * @param int $playerId
-     *
-     * @return bool
+     * @inheritDoc
      */
-    public function adminForceTeamChangeById(int $playerId): bool
+    public function adminForceTeamChangeById(int $playerId) : bool
     {
         return $this->_consoleCommand('AdminForceTeamChangeById', $playerId, 'Forced team change for player ');
     }
 
     /**
-     * AdminDemoteCommander command.
-     * Demotes a player from the commander slot
-     * by providing the name or steamid.
-     *
-     * @param string $playerName
-     *
-     * @return bool
+     * @inheritDoc
      */
     //public function adminDemoteCommander(string $nameOrSteamId) : bool
     //{
@@ -309,13 +235,7 @@ class RCONWorkerCommandRunner implements ServerCommandRunner
     //}
 
     /**
-     * AdminDemoteCommanderById command.
-     * Demotes a player from the commander slot
-     * by providing the ingame Player id.
-     *
-     * @param int $playerId
-     *
-     * @return bool
+     * @inheritDoc
      */
     //public function adminDemoteCommanderById(int $playerId) : bool
     //{
@@ -323,75 +243,43 @@ class RCONWorkerCommandRunner implements ServerCommandRunner
     //}
 
     /**
-     * AdminDisbandSquad command.
-     * Disbands a Squad by providing the Team id  / index & Squad id / index.
-     *
-     * @param int $teamId
-     * @param int $squadId
-     *
-     * @return bool
+     * @inheritDoc
      */
-    public function adminDisbandSquad(int $teamId, int $squadId): bool
+    public function adminDisbandSquad(int $teamId, int $squadId) : bool
     {
-        return $this->_consoleCommand('AdminDisbandSquad', $teamId.' '.$squadId, 'Remote admin disbanded squad '.$squadId.' on team '.$teamId.', named "');
+        return $this->_consoleCommand('AdminDisbandSquad', $teamId . ' ' . $squadId, 'Remote admin disbanded squad ' . $squadId . ' on team ' . $teamId . ', named "');
     }
 
     /**
-     * AdminRemovePlayerFromSquad command.
-     * Removes a Player from his Squad by providing
-     * the Player name.
-     *
-     * @param string $playerName
-     *
-     * @return bool
+     * @inheritDoc
      */
-    public function adminRemovePlayerFromSquad(string $playerName): bool
+    public function adminRemovePlayerFromSquad(string $playerName) : bool
     {
         return $this->_consoleCommand('AdminRemovePlayerFromSquad', $playerName, 'Player ', ' was removed from squad');
     }
 
     /**
-     * AdminRemovePlayerFromSquadById command.
-     * Removes a player from his Squad by providing
-     * the ingame Player id.
-     *
-     * @param int $playerId
-     *
-     * @return bool
+     * @inheritDoc
      */
-    public function adminRemovePlayerFromSquadById(int $playerId): bool
+    public function adminRemovePlayerFromSquadById(int $playerId) : bool
     {
         return $this->_consoleCommand('AdminRemovePlayerFromSquadById', $playerId, 'Player ', ' was removed from squad');
     }
 
     /**
-     * AdminWarn command.
-     * Warns a Player by providing his name / steamid
-     * and a message.
-     *
-     * @param string $nameOrSteamId
-     * @param string $warnReason
-     *
-     * @return bool
+     * @inheritDoc
      */
-    public function adminWarn(string $nameOrSteamId, string $warnReason): bool
+    public function adminWarn(string $nameOrSteamId, string $warnReason) : bool
     {
-        return $this->_consoleCommand('AdminWarn', $nameOrSteamId.' '.$warnReason, 'Remote admin has warned player ');
+        return $this->_consoleCommand('AdminWarn', $nameOrSteamId . ' ' . $warnReason, 'Remote admin has warned player ');
     }
 
     /**
-     * AdminWarnById command.
-     * Warns a Player by providing his ingame Player id
-     * and a message.
-     *
-     * @param int    $playerId
-     * @param string $warnReason
-     *
-     * @return bool
+     * @inheritDoc
      */
-    public function adminWarnById(int $playerId, string $warnReason): bool
+    public function adminWarnById(int $playerId, string $warnReason) : bool
     {
-        return $this->_consoleCommand('AdminWarnById', $playerId.' '.$warnReason, 'Remote admin has warned player ');
+        return $this->_consoleCommand('AdminWarnById', $playerId . ' ' . $warnReason, 'Remote admin has warned player ');
     }
 
     /**
@@ -461,9 +349,7 @@ class RCONWorkerCommandRunner implements ServerCommandRunner
     }
 
     /**
-     * Disconnects the runner from any squad server instance.
-     *
-     * @return void
+     * @inheritDoc
      */
     public function disconnect(): void
     {
