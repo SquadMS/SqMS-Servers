d0d1c98c3044899b5c11ea16860b93ebcf9bbb2a
Patrick <privat@skyraptor.eu>
2022-06-09 22:42:21 +0200

    Make use of SquadMSUser model

--- src/Services/ServerQueryService.php
+++ src/Services/ServerQueryService.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use SquadMS\Foundation\Auth\SteamUser;
 use SquadMS\Foundation\Jobs\FetchUsers;
+use SquadMS\Foundation\Models\SquadMSUser;
 use SquadMS\Foundation\Repositories\UserRepository;
 use SquadMS\Servers\Data\ServerQueryResult;
 
@@ -43,7 +44,7 @@ class ServerQueryService
             /* Bulk create the users shallowly and check if anything has been created or updated */
             if (UserRepository::createOrUpdateBulk($steamUsers, true)) {
                 /* Query and get the created users */
-                return UserRepository::getUserModelQuery()->whereIn('steam_id_64', $steamIds)->get();
+                return SquadMSUser::whereIn('steam_id_64', $steamIds)->get();
             }
         }
 
