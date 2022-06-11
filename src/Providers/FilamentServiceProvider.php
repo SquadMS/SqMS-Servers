df745a9e36980f4d70a30bb9b52037cbb08e3465
Patrick <privat@skyraptor.eu>
2022-06-11 19:18:14 +0200

    Fix namespaces and configure package name

--- src/Providers/FilamentServiceProvider.php
+++ src/Providers/FilamentServiceProvider.php
@@ -3,10 +3,17 @@
 namespace SquadMS\Servers\Providers;
 
 use Filament\PluginServiceProvider;
- 
+use Spatie\LaravelPackageTools\Package;
+use SquadMS\Servers\Filament\Resources\ServerResource;
+
 class FilamentServiceProvider extends PluginServiceProvider
 {
     protected array $resources = [
         ServerResource::class,
     ];
+
+    public function configurePackage(Package $package): void
+    {
+        $package->name('sqms-servers');
+    }
 }
