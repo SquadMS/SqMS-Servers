4b03e9c3f48a023f427e6e39cd6be440ff23cbe3
Patrick <privat@skyraptor.eu>
2022-06-13 20:11:42 +0200

    Update population.blade.php

--- resources/views/components/player-list/population.blade.php
+++ resources/views/components/player-list/population.blade.php
@@ -2,7 +2,7 @@
     @if ($server->last_query_result->population())
         @foreach ($server->last_query_result->population()->getTeams() as $team)
             <div class="col-12 col-md-6">
-                <x-sqms-default-theme::player-list.team :team="$team" :server="$server" />
+                <x-sqms-servers::player-list.team :team="$team" :server="$server" />
             </div>
         @endforeach
     @elseif ($server->has_rcon_data)
