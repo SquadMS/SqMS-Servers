6d05f4b571944ea601062b240611f26264b6ad28
Patrick <privat@skyraptor.eu>
2022-06-13 20:13:35 +0200

    Update sqms-servers.php

--- config/sqms-servers.php
+++ config/sqms-servers.php
@@ -36,6 +36,15 @@ return [
                 'controller'  => \SquadMS\Servers\Http\Controllers\ServersController::class,
                 'executor'    => 'show',
                 'localized'   => true,
+            ],
+            'server-population' => [
+                'type' => 'get',
+                'name' => 'server',
+                'path' => 'servers/{server}/population',
+                'middlewares' => [],
+                'controller' => \SquadMS\DefaultTheme\Http\Controllers\ServerController::class,
+                'executor' => 'population',
+                'localized' => true,
             ]
         ],
     ],
