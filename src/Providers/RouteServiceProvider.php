8642717f2a132fdd89d4cabf7b25585931496eb0
Patrick <privat@skyraptor.eu>
2022-06-15 22:15:51 +0200

    Register routes in booted

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -18,23 +18,25 @@ class RouteServiceProvider extends ServiceProvider
         /* Middlewares */
         Route::aliasMiddleware('sqms-worker-auth', \SquadMS\Servers\Http\Middleware\WorkerAuth::class);
 
-        /* Routes */
-        $routesPath = __DIR__.'/../../routes';
+        $this->app->booted(function() {
+            /* Routes */
+            $routesPath = __DIR__.'/../../routes';
 
-        /* WEB routes */
-        Route::group([
-            'prefix'     => Config::get('sqms-servers.routes.prefix'),
-            'middleware' => Config::get('sqms-servers.routes.middleware'),
-        ], function () use ($routesPath) {
-            $this->loadRoutesFrom($routesPath.'/web.php');
-        });
+            /* WEB routes */
+            Route::group([
+                'prefix'     => Config::get('sqms-servers.routes.prefix'),
+                'middleware' => Config::get('sqms-servers.routes.middleware'),
+            ], function () use ($routesPath) {
+                $this->loadRoutesFrom($routesPath.'/web.php');
+            });
 
-        /* API routes */
-        Route::group([
-            'prefix'     => 'api',
-            'middleware' => 'api',
-        ], function () use ($routesPath) {
-            $this->loadRoutesFrom($routesPath.'/api.php');
+            /* API routes */
+            Route::group([
+                'prefix'     => 'api',
+                'middleware' => 'api',
+            ], function () use ($routesPath) {
+                $this->loadRoutesFrom($routesPath.'/api.php');
+            });
         });
     }
 }
