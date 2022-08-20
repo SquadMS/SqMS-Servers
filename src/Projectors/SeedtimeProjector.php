f9980be309930a26974cb5bcdabf1ed8dc38c664
Patrick <privat@skyraptor.eu>
2022-08-20 22:55:53 +0200

    Make use of new configurable seeding counts, Updated RCON abstraction lib,

--- src/Projectors/SeedtimeProjector.php
+++ src/Projectors/SeedtimeProjector.php
@@ -12,7 +12,7 @@ class SeedtimeProjector extends Projector
 {
     public function onServerQueried(ServerQueried $event): void
     {
-        if ($event->serverQueryResult->count && $event->serverQueryResult->count < 40) {
+        if ($event->serverQueryResult->count && $event->serverQueryResult->count >= $event->serverQueryResult->server->start_seeding && $event->serverQueryResult->count < $event->serverQueryResult->server->stop_seeding) {
             PlayerServerInfo::where('server_id', $event->serverQueryResult->server->id)
                 ->whereHas('user', function (Builder $query) use ($event) {
                     $query->whereIn('steam_id_64', $event->serverQueryResult->steamIds());
